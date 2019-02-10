5a6c5e22a5f1842b574ff7fc364b1b360482a0d0
Will Vanvelsor <wvanvels@uncc.edu>
2019-02-10 19:49:07 +0000

    RubyBasics - Part 3 rspec enabled

--- lib/rubybasics2.rb
+++ lib/rubybasics2.rb
@@ -3,15 +3,21 @@
 # Part I
 def hello(name)
   # YOUR CODE HERE
-  return "Hello, " + name
+  "Hello, " + name
+
 end
 
 # Part II
 def starts_with_consonant? s
   # YOUR CODE HERE
+  !!(s[0] =~ /[bcdfghjklmnprstvwxyz]+/i)
 end
 
 # Part III
 def binary_multiple_of_4? s
   # YOUR CODE HERE
+  if s =~ /^[0-1]+$/
+return s.to_i(2) % 4 == 0 unless s == "0"
+  end
+false
 end
